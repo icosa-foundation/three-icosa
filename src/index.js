ddd89db7055a611607776d8391e1201c5225e10d
Mike Nisbet <nisbet.mr@gmail.com>
2022-04-23 15:24:37 +0100

    Export TiltShaderLoader

--- src/index.js
+++ src/index.js
@@ -12,4 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+export { TiltShaderLoader } from './TiltShaderLoader';
 export { GLTFGoogleTiltBrushMaterialExtension } from './loader/GOOGLE_tilt_brush_material';
